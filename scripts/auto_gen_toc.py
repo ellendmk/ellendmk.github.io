2047381f66e6206b2d64fe559063dcc0945a331b
Ellen Rudman <ellendemellokoch@gmail.com>
2023-07-10 15:24:17 +0100

    Update auto_gen_toc.py

--- scripts/auto_gen_toc.py
+++ scripts/auto_gen_toc.py
@@ -5,36 +5,64 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--fname', type=str, help='file name to add table of contents to')
 
+def get_structure(content):
+    heading_locs = [m.start() for m in re.finditer('#+', content)]
+    list_struct = []
+    for i in heading_locs:
+        text = content[i:].split('\n')[0]
+        list_struct.append(text)
+        
+    return heading_locs, list_struct
+
+
+def gen_toc(structure):
+    prev = 1
+    toc = '''
+<div class="DivTableOfContentsLink toc">
+    <ul class="toc__list">
+        <ul class="toc__sublist">'''
+    for k in structure:
+        cur = sum([1 for c in k if c=='#'])
+        if cur==prev:
+            toc += f'''
+    <li><a href="#{k.replace(" ","-")}">{k.replace("#","").strip()}</a></li>'''
+        elif cur+1==prev:
+            toc += f'''
+    </ul>
+    <li><a href="#{k.replace(" ","-")}">{k.replace("#","").strip()}</a></li>'''
+
+        elif cur==prev+1:
+            toc += f'''
+    <ul class="toc__sublist">
+        <li><a href="#{k.replace(" ","-")}">{k.replace("#","").strip()}</a></li>'''
+        prev=cur
+
+    toc += '''
+        </ul>
+    </ul>
+</div>'''
+    return toc
+
+def update_content(content, indices, toc, links):
+    content_new = content[0:indices[0]] + '\n' + toc + '\n' + links[0] +'\n\n'
+    for i in range(1,len(indices)):
+        content_new +=  '\n' + content[indices[i-1]:indices[i]] + '\n' + links[i] + '\n' 
+
+    return content_new
 
 def generate_toc(fname):
 
     with open(fname) as f:    
         content = f.read()
-        heading_locs = [m.start() for m in re.finditer('#+', content)]
-        structure = {}
-        level_prev = 1
-        parent = []
-        for i in heading_locs:
-            text = content[i:].split('\n')[0]
-            level_cur = len([m.start() for m in re.finditer('#', text)])
-            text = text.replace('#','').strip()
-            
-            if level_cur==1:
-                parent = parent[0:-2]
-            if level_cur>level_prev:
-                parent.append(old_text)
-            
-            if level_cur-level_prev>=1 or parent is not None:
-                temp_structure = structure
-                for p in parent:
-                    temp_structure = temp_structure[p]
-                temp_structure.update({text:{}})
-            elif parent is None:
-                structure.update({text:{}})
-
-            old_text = text
-            print(structure)
-            level_prev = level_cur
+        indices, structure = get_structure(content)
+        toc = gen_toc(structure)
+        links = [f'<p id = "{i.replace("#","").strip().replace(" ","-")}"></p>' for i in structure]
+        content_updated = update_content(content, indices, toc, links)
+        
+        with  open(fname.replace('.md','_new.md'),'w+') as fnew: 
+            fnew.write(content_updated)
+        
+    
 
 if __name__ == "__main__":
     args = parser.parse_args()
